4a85da433fbd283244a455ab38588d2a3ef9fdd8
IidaPeltonen <n0peii00@students.oamk.fi>
2021-10-18 22:37:11 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
 import './App.css';
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from 'react'
 import axios from 'axios';
 
-const URL= 'http://localhost/todo/index.php';
+const URL = 'http://localhost/todo/';
 
-function App() {  const [tasks, setTasks] = useState([]);
+function App() {
+  const [tasks, setTasks] = useState([]);
   const [task, setTask] = useState('');
-  
+
   useEffect(() => {
-    axios.get(URL)
+    axios.get(URL + 'index.php')
       .then((response) => {
+        console.log(response.data)
         setTasks(response.data)
       }).catch(error => {
         alert(error.response ? error.response.data.error : error);
       })
   }, [])
 
-  function save (e) {
-    e.preventDefault()
-    const json = JSON.stringify({description: task})
-    axios.post(URL + 'add.php', json, {
-        headers: {
-          'Content-Type' : 'application/json'
-        }
-      })
-      .then(response => {
-        setTasks(tasks => [...tasks, response.data]);
-        setTask('');
-      }).catch(error => {
-        alert(error.response.data.error)
-      });
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({description:task})
+    axios.post(URL + 'add.php',json,{
+      headers: {
+        'Content-Type' : 'applicationJ/json'
+      }
+    })
+    .then((response) => {
+      setTasks(tasks => [...tasks,response.data]);
+      setTask('');
+    }).catch (error => {
+      alert(error.response.data.error)
+    })
   }
+
+  function remove(id) {
+    const json= JSON.stringify({id:id})
+    axios.post(URL + 'delete.php', json, {
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      const newListWithoutRemoved = tasks.filter((item) => item.id !== id);
+      setTasks(newListWithoutRemoved);
+    }).catch (error => {
+      alert(error.response ? error.response.data.error : error);
+    });
+  }
+
 
   return (
     <div className="container">
@@ -39,16 +57,18 @@ function App() {  const [tasks, setTasks] = useState([]);
         <label>New task</label>
         <input value={task} onChange={e => setTask(e.target.value)} />
         <button>Save</button>
-      </form>  
+      </form>
       <ol>
         {tasks?.map(task => (
-          <li key={task.id}>{task.description}</li>
+          <li key={task.id}>{task.description}&nbsp;
+            <a herf="#" className="delete" onClick={() => remove(task.id)}>
+              Delete
+            </a>
+          </li>
         ))}
       </ol>
     </div>
   );
 }
-
-
 
 export default App;
